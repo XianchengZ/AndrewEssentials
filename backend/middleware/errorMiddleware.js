e9b60c06eda992e67b135fe856ba4b17cf1b1ecc
Andrew Zang <andrewz020529@gmail.com>
2021-10-12 20:05:57 -0400

    store created

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,9 +1,11 @@
+// notFound handles url that are not valid
 const notFound = (req, res, next) => {
   const error = new Error(`Not Found - ${req.originalUrl}`);
   res.status(404);
   next(error);
 };
 
+// errorHandler handles cases where there is an error
 const errorHandler = (err, req, res, next) => {
   const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
   res.status(statusCode);
