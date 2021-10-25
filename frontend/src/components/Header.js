4df96446bb846e949ba12a04be677e14ec904d58
Andrew Zang <andrewz020529@gmail.com>
2021-10-24 20:37:46 -0400

    user register feature added

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Container, Navbar, Nav, NavDropdown } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
-//import { USER_LOGOUT } from '../constants/userConstants';
 import { logout } from '../actions/userActions';
 
 const Header = () => {
