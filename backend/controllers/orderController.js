1cff9cdfbf18af82a16a2b61e356b2122e0b6172
Andrew Zang <andrewz020529@gmail.com>
2021-10-28 07:15:27 -0400

    show orders on profile

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -67,4 +67,13 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
 	}
 });
 
-export { addOrderItems, getOrderById, updateOrderToPaid };
+//  @description: get logged in user orders
+//  @route  get /api/orders/myorders
+//  @access Private
+const getMyOrders = asyncHandler(async (req, res) => {
+	const orders = await Order.find({ user: req.user._id });
+
+	res.json(orders);
+});
+
+export { addOrderItems, getOrderById, updateOrderToPaid, getMyOrders };
