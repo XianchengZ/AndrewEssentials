7bfcdad96c782d9e16e20c02a277cadacff68a81
Andrew Zang <andrewz020529@gmail.com>
2021-10-29 23:50:25 -0400

    admin user list created

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -7,7 +7,8 @@ import {
 	userDetailsReducer,
 	userLoginReducer,
 	userRegisterReducer,
-	userUpdateProfileReducer
+	userUpdateProfileReducer,
+	userListReducer
 } from './reducers/userReducers';
 import { orderCreateReducer, orderDetailsReducer, orderPayReducer, orderListMyReducer } from './reducers/orderReducers';
 
@@ -19,6 +20,7 @@ const reducer = combineReducers({
 	userRegister: userRegisterReducer,
 	userDetails: userDetailsReducer,
 	userUpdateProfile: userUpdateProfileReducer,
+	userList: userListReducer,
 	orderCreate: orderCreateReducer,
 	orderDetails: orderDetailsReducer,
 	orderPay: orderPayReducer,
